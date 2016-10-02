648cc01a75286630f09507277319a87e6e17c573
陈宇峰 <yufengcode@gmail.com>
2016-10-02 18:00:14 +0800

    init

--- BinaryTreeLeafCount/main.cpp
+++ BinaryTreeLeafCount/main.cpp
@@ -8,8 +8,82 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+typedef struct TreeNode{
+    int data;
+    struct TreeNode *lChild;
+    struct TreeNode *rChild;
+}Node;
+
+/**
+ *  求二叉树叶子节点的个数
+ */
+
+void CreateBinaryTree(Node **node)
+{
+    int num;
+    cin >> num;
+
+    if (num == -1)
+    {
+        return;
+    }
+
+    *node = new Node;
+    (*node)->data = num;
+    (*node)->lChild = NULL;
+    (*node)->rChild = NULL;
+
+    cout << "请输入" << num << "的左孩子的节点：";
+    CreateBinaryTree(&((*node)->lChild));
+    cout << "请输入" << num << "的右孩子的节点：";
+    CreateBinaryTree(&((*node)->rChild));
+}
+
+static int leafCount = 0;
+
+int LeafCountBinaryCount(Node *node)
+{
+    if (node == NULL)
+    {
+        return leafCount;
+    }
+
+    if (node ->lChild == NULL && node ->rChild == NULL)
+    {
+        leafCount++;
+    }
+
+    LeafCountBinaryCount(node->lChild);
+    LeafCountBinaryCount(node->rChild);
+
+    return leafCount;
+}
+
+int main(int argc, const char * argv[])
+{
+    Node *node;
+
+    cout << "请输入二叉树的节点，输入-1结束:"<< endl;
+    CreateBinaryTree(&node);
+
+    LeafCountBinaryCount(node);
+    cout << leafCount << endl;
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
